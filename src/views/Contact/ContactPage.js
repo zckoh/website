37503f7267a2a82825fd0ba78300fe9f87b9e3ba
zckoh <zckoh96@gmail.com>
2020-08-21 08:41:10 +0100

    fix liinkedin link on contact page

--- src/views/Contact/ContactPage.js
+++ src/views/Contact/ContactPage.js
@@ -26,7 +26,7 @@ const ContactPage = () => {
                             <a href="https://github.com/zckoh" target="_blank" rel="noopener noreferrer" className="text pr-3" style={{ fontSize: `30px` }}>
                                 <MDBIcon fab icon="github" />
                             </a>
-                            <a href="https://github.com/zckoh" target="_blank" rel="noopener noreferrer" className="text pr-3" style={{ fontSize: `30px` }}>
+                            <a href="http://www.linkedin.com/in/zackoh" target="_blank" rel="noopener noreferrer" className="text pr-3" style={{ fontSize: `30px` }}>
                                 <MDBIcon fab icon="linkedin" />
                             </a>
                             <a href="https://www.kaggle.com/zckoh20" target="_blank" rel="noopener noreferrer" className="text" style={{ fontSize: `30px` }}>
